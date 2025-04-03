f34f9460b329b00787af92baefba81ec9fe1c6c5
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2025-04-03 12:12:56 +0530

    . Student Exam Evaluator

--- src/Document4/StudentExamEvaluator.java
+++ src/Document4/StudentExamEvaluator.java
@@ -1,7 +1,14 @@
 package Document4;
 
 import java.util.Scanner;
-
+/*
+Q. Student Exam Evaluator (Skip Invalid Marks)
+ Scenario: Marks cannot be negative, so skip them!
+ Task:
+ Take 5 subject marks as input.
+ If negative, continue (skip) and move to the next subject.
+ Show total marks of valid subjects.
+ */
 public class StudentExamEvaluator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
