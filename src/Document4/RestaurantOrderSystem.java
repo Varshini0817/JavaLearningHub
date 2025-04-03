5683f6fe180203dedddaef93b75dab5d10ac64ea
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2025-04-03 12:39:22 +0530

    Restaurant Order System

---
+++ src/Document4/RestaurantOrderSystem.java
@@ -0,0 +1,28 @@
+package Document4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+Q. Restaurant Order System (Stop Taking Orders When Kitchen Closes)
+ Scenario: Orders stop after 10 PM!
+ Task:
+ Continuously take orders.
+ If time hits 10 PM → break & stop orders.
+ Display final order list.
+ */
+public class RestaurantOrderSystem {
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<>();
+        int i = 0;
+        do {
+
+        } while (i > 0);
+
+
+
+    }
+}
+
+
